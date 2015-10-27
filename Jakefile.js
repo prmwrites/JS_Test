5baebe09353ea76f73494cc17c5256b3c9f85e37
Patrick Morris <prmwrites@gmail.com>
2015-10-27 18:52:39 -0400

    Our first test-driven error method

--- Jakefile.js
+++ Jakefile.js
@@ -22,7 +22,7 @@ task("lint", [], function() {
 desc("Test everything");
 task("test", [], function() {
 	
-	var reporter = require("nodeunit").reporters.minimal;
+	var reporter = require("nodeunit").reporters["default"];
 	reporter.run(['src/server/_server_test.js']);
 });
 
