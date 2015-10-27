77542f2466991dc7e34ef3858daebc1da92e71f7
Patrick Morris <prmwrites@gmail.com>
2015-10-27 18:13:05 -0400

    Set up continuous integration script

--- Jakefile.js
+++ Jakefile.js
@@ -18,6 +18,18 @@ task("lint", [], function() {
 	lint.validateFileList(files.toArray(), options, {});
 });
 
+desc("Integrate");
+task("integrate", [ "default" ], function() {
+	console.log("1. Make sure 'git status' is clean.");
+	console.log("2. Build on the integration box.");
+	console.log("	a. walk over to the integration box");
+	console.log("	b. 'git pull'");
+	console.log("	c. 'jake'.");
+	console.log("3. 'git checkout integration'");
+	console.log("4. 'git merge master --no-ff --log'");
+	console.log("5. 'git checkout master'");
+});
+
 function nodeLintOptions() {
  return {
 		bitwise: true,
--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 WeeWikiPaint
 
+to build on a fresh contiunous integration machine:
+- Install node.js
+- Install git
+- Clone Respository
+
 To build:
 - assumes prescense of Node.js
 - run 'jake'
