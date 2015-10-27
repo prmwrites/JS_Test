dddcaeaa31992d161ea7df722aafe72c92bf7a52
Patrick Morris <prmwrites@gmail.com>
2015-10-27 17:10:33 -0400

    Finished automated build

---
+++ Jakefile.js
@@ -0,0 +1,12 @@
+task("default", function() {
+	console.log("default");
+});
+
+desc("example");
+task("example", [ "dependency"], function() {
+	console.log("example task");
+});
+
+task("dependency", function() {
+	console.log("dependency");
+});
